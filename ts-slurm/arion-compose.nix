7a47ca7218c906e1ccd186362ace6105b18b38cf
Olivier Richard <olivier.richard@imag.fr>
2019-09-26 20:03:11 +0200

    Code refactoring

--- ts-slurm/arion-compose.nix
+++ ts-slurm/arion-compose.nix
@@ -1,49 +1,47 @@
-
+{ pkgs, lib, ... }:
 let 
-    slurmconfig = {
-      controlMachine = "control";
-      nodeName = [ "node[1] CPUs=1 State=UNKNOWN" ];
+  slurmconfig = {
+  controlMachine = "control";
+  nodeName = [ "node[1] CPUs=1 State=UNKNOWN" ];
       partitionName = [ "debug Nodes=node[1] Default=YES MaxTime=INFINITE State=UP" ];
       #extraConfig = ''
       #  AccountingStorageHost=dbd
       #  AccountingStorageType=accounting_storage/slurmdbd
-      #'';
-    };
+  #'';
+  };
+
+  common = {
+    nixos.useSystemd = true;
+    nixos.configuration.boot.tmpOnTmpfs = true;
+    nixos.configuration.environment.systemPackages = with pkgs; [python3];
+    service.useHostStore = true;
+    nixos.configuration.services.batsky = {enable = true; controller="submit";};
+    service.volumes = [ "/home/auguste/dev/batsky:/batsky" ];
+  };
+
+  addCommon = x: lib.recursiveUpdate x common;
+
 in
 
 {
-
-  docker-compose.services.node1 = { pkgs, ... }: {
-    nixos.useSystemd = true;
-    nixos.configuration.boot.tmpOnTmpfs = true;
+  docker-compose.services.node1 = addCommon {
+    service.hostname="node1";  
     nixos.configuration.services.ts-slurm  = {
       client.enable = true;
     } // slurmconfig;
-    nixos.configuration.services.batsky = {enable = true; controller="submit";};
-    service.useHostStore = true;
-    service.hostname="node1";
   };
-  
-  docker-compose.services.control = { pkgs, ... }: {
-    nixos.useSystemd = true;
-    nixos.configuration.boot.tmpOnTmpfs = true;
+ 
+  docker-compose.services.control = addCommon {
+    service.hostname="control";
     nixos.configuration.services.ts-slurm  = {
       server.enable = true;
     } // slurmconfig;
-    nixos.configuration.services.batsky = {enable = true; controller="submit";};
-    service.hostname="control";
-    service.useHostStore = true;
-  };
+  }; 
   
-  docker-compose.services.submit = { pkgs, ... }: {
-    nixos.useSystemd = true;
-    nixos.configuration.boot.tmpOnTmpfs = true;
+  docker-compose.services.submit = addCommon {
+    service.hostname="submit";
     nixos.configuration.services.ts-slurm  = {
       enableStools = true;
     } // slurmconfig;
-    nixos.configuration.services.batsky = {enable = true; controller="submit";};
-    service.hostname="submit";
-    service.useHostStore = true;
-    
   };
 }
